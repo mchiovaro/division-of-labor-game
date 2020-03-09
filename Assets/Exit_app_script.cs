49d95b88fa28507762f6c81d0ea2f17616a11d7c
Megan Chiovaro <megan.chiovaro@uconn.edu>
2020-03-09 13:29:25 -0400

    Change to FixedUpdate() and add documentation

--- Assets/Exit_app_script.cs
+++ Assets/Exit_app_script.cs
@@ -5,29 +5,34 @@ using System.Text;
 using System.IO;
 using UnityEngine.UI;
 
-
 public class Exit_app_script : MonoBehaviour
 {
-    float ScToWRatio;
-    GameObject work_cells, drop_cells, spawn_cells;   // Start is called before the first frame update
+    float ScToWRatio; // some camera ratio?
+    GameObject work_cells, drop_cells, spawn_cells; 
 
     public int ParticipantNumber;
     public string RA_initials;
     public int current_trial = 0;
 
-    private const int MAX_PELLETS = 20;
+    private const int MAX_PELLETS = 20; // 20 cells
 
+    //
     private List<int> conditions_ = new List<int>(6);
     private const int CONDITION_EXPERIMENT_OVER = 0;
+
+    // rates of incoming food pellets
     private const int CONDITION_CONSTANT_RATE = 1;
     private const int CONDITION_INCREASING_RATE = 2;
     private const int CONDITION_RANDOM_RATE = 3;
 
+    // initial conditions
     private int current_cond = 0;
     private bool practice_mode = true;
 
+    // allows for the rate of spawn to be changed in condition checkCondition()
     private List<float> spawn_rate = new List<float>(new float[MAX_PELLETS]);
 
+    // counter for number of dropped pellets
     private List<GameObject> dropped_pellets = new List<GameObject>();
 
     private bool running_ = false;
@@ -42,12 +47,14 @@ public class Exit_app_script : MonoBehaviour
     public Text ui_text;
     public RawImage ui_image;
 
+    // 
     public struct SaveData
     {
-        public List<Vector3> allPositions;
-        public List<float> allTimeStamps;
+        public List<Vector3> allPositions; //
+        public List<float> allTimeStamps; // 
     }
 
+    //
     public struct PelletData
     {
         public List<string> pelletEvent;
@@ -55,25 +62,25 @@ public class Exit_app_script : MonoBehaviour
     }
 
     public List<PelletData> pellData = new List<PelletData>();
- //   public List<List<string>> pelletEvent;
-
-  //  public List<SaveData2> saveData2 = new List<SaveData2>();
+    // public List<List<string>> pelletEvent;
+    // public List<SaveData2> saveData2 = new List<SaveData2>();
 
     public SaveData data_beeFree, data_beeRestrict;
     private bool quitting = false;
 
     private float timeIni = 0;
 
+    // Start is called before the first frame update
     void Start()
     {
-
+        // create list for saving pellet data
         for (int ii = 0; ii < MAX_PELLETS; ii++)
         {
             PelletData pD;
             pD.pelletEvent = new List<string>();
             pD.frameNum = new List<int>();
 
-            pellData.Add(pD);
+            pellData.Add(pD); // 
         }
 
      //   for (int pelInd = 0; pelInd < 500000; pelInd++)
@@ -83,7 +90,6 @@ public class Exit_app_script : MonoBehaviour
      //       saveData2.Add(sd2);
      //   }
 
-
         loadParams();
 
         checkCondition();
@@ -92,11 +98,10 @@ public class Exit_app_script : MonoBehaviour
         ui_text.text = "Ready to practice?";
 
         //allocate memory for Data to save
-        data_beeFree.allPositions = new List<Vector3>(100000);
-        data_beeFree.allTimeStamps = new List<float>(100000);
-
-        data_beeRestrict.allPositions = new List<Vector3>(100000);
-        data_beeRestrict.allTimeStamps = new List<float>(100000);
+        data_beeFree.allPositions = new List<Vector3>(100000); // old bee
+        data_beeFree.allTimeStamps = new List<float>(100000); // old bee
+        data_beeRestrict.allPositions = new List<Vector3>(100000); // young bee
+        data_beeRestrict.allTimeStamps = new List<float>(100000); // young bee
 
         //find all the environment gameObjects and assign them to variables
         work_cells = GameObject.FindGameObjectWithTag("work_row_middle");
@@ -105,8 +110,10 @@ public class Exit_app_script : MonoBehaviour
         beeFree = GameObject.FindGameObjectWithTag("bee_free");
         beeRestrict = GameObject.FindGameObjectWithTag("bee_restricted");
 
+        // set camera ratio
         ScToWRatio = 1080 / (2 * Camera.main.orthographicSize);
 
+        // ?
         for (int ii = 0; ii < work_cells.transform.childCount; ii++)
             work_cells.transform.GetChild(ii).position = new Vector3(0, +ii * 108 / ScToWRatio - Camera.main.orthographicSize + 108 / (2 * ScToWRatio));// + 3 * 108 / (2 * ScToWRatio));
 
@@ -119,11 +126,12 @@ public class Exit_app_script : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        // ?
         float timestamp = Time.time - timeIni;
 
+        // ?
         if (running_)
         {
             current_frame++;
@@ -154,7 +162,6 @@ public class Exit_app_script : MonoBehaviour
                     if (!practice_mode){                  
                         pel_.GetComponent<PelletScript>().saveToBuffer("SPAWN");
                     }
-
                 }
             }
 
@@ -163,9 +170,9 @@ public class Exit_app_script : MonoBehaviour
 
             data_beeFree.allPositions.Add(beeFree.transform.position);
             data_beeRestrict.allPositions.Add(beeRestrict.transform.position);
-
            
         }
+
         else if (!(current_cond == conditions_.Count - 1))
         {
             if (Input.GetKey("space"))
@@ -182,6 +189,7 @@ public class Exit_app_script : MonoBehaviour
             }
 
         }
+
         else
         {
             if (Input.GetKey("escape")) Application.Quit();
@@ -189,6 +197,7 @@ public class Exit_app_script : MonoBehaviour
 
     }
 
+    // drops a pellet
     public void addPellet(GameObject pell_)
     {
 
@@ -201,12 +210,12 @@ public class Exit_app_script : MonoBehaviour
         {
             running_ = false;
 
-            // 
+            // save data if it's not from practice
             if (!practice_mode){
                 saveToDisk();
             }
 
-            //RESET EVERYTHING
+            //RESETS EVERYTHING
             dropped_pellets.Clear();
 
             GameObject[] allPellets = GameObject.FindGameObjectsWithTag("pellet");
@@ -243,7 +252,7 @@ public class Exit_app_script : MonoBehaviour
             if (practice_mode){
                 practice_mode = false;
             }
-            else{
+            else {
                 current_cond++;
                 current_trial++;
             }            
@@ -264,29 +273,36 @@ public class Exit_app_script : MonoBehaviour
     {
         switch (conditions_[current_cond])
         {
+            // constant rate condition
             case CONDITION_CONSTANT_RATE:
 
                 ui_image.enabled = true;
                 ui_text.text = "Round Finished. \n Ready for next round?";
 
+                // keeps spawn rate at 5.0f
                 for (int ii = 0; ii < MAX_PELLETS; ii++)
                     spawn_rate[ii] = 5.0f;//.Add(5.0f);
 
                 break;
 
+            // monotinic increasing condition
             case CONDITION_INCREASING_RATE:
                 ui_image.enabled = true;
                 ui_text.text = "Round Finished. \n Ready for next round?";
 
+                // starts at 7.0f and adds 0.25f each time a pellet spawns
+                // why is there a minus here?
                 for (int ii = 0; ii < MAX_PELLETS; ii++)
                 {
                     spawn_rate[ii] = 7.0f - ii * 0.25f;
 
+                    // reset to 0.25f if current rate is less than it
                     if (spawn_rate[ii] <= 0.25f) spawn_rate[ii] = 0.25f;
                 }
 
                 break;
 
+            // random rate condition
             case CONDITION_RANDOM_RATE:
                 ui_image.enabled = true;
                 ui_text.text = "Round Finished. \n Ready for next round?";
@@ -295,6 +311,7 @@ public class Exit_app_script : MonoBehaviour
                     spawn_rate[ii] = Random.Range(0.5f, 6.0f);
                 break;
 
+            // end
             case CONDITION_EXPERIMENT_OVER:
 
                 ui_image.enabled = true;
@@ -308,6 +325,7 @@ public class Exit_app_script : MonoBehaviour
 
     }
 
+    // save data here
     void saveToDisk()
     {
         string filetitle = RA_initials + "-" + ParticipantNumber;
@@ -373,6 +391,7 @@ public class Exit_app_script : MonoBehaviour
 
     }
 
+    // not used anywhere; can this get nixed?
     void saveAndQuit()
     {
         quitting = true;
@@ -395,12 +414,12 @@ public class Exit_app_script : MonoBehaviour
     void loadParams()
     {
 
+        // what is this doing?
         int partOffSet = 1 + (ParticipantNumber - 1) * 6;
 
         TextAsset iniFile = Resources.Load<TextAsset>("loadParams/participant_sheet2");
 
         string[] row = iniFile.text.Split(new char[] { '\n' });
-
 
         for (int ii = 0; ii < 6; ii++)
         {
