6b565dfdaf7812ca0fd62395e2ca1815e9263ec8
Megan Chiovaro <megan.chiovaro@uconn.edu>
2020-03-10 21:24:45 -0400

    Fixed movement data saving and even more documentation

--- Assets/Exit_app_script.cs
+++ Assets/Exit_app_script.cs
@@ -1,22 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using System.Text;
 using System.IO;
 using UnityEngine.UI;
 
 public class Exit_app_script : MonoBehaviour
 {
     float ScToWRatio; // some camera ratio?
-    GameObject work_cells, drop_cells, spawn_cells; 
+    GameObject work_cells, drop_cells, spawn_cells;
+    public GameObject foodPrefab, beeFree, beeRestrict;
 
+    // input parameters
     public int ParticipantNumber;
     public string RA_initials;
+
+    // trial counter
     public int current_trial = 0;
 
-    private const int MAX_PELLETS = 20; // 20 pellets for 20 cells
+    // max number of pellets based on number of cells available
+    private const int MAX_PELLETS = 20;
 
-    //
+    // initialize conditions list
     private List<int> conditions_ = new List<int>(6);
 
     // ending experiment condition
@@ -28,9 +31,9 @@ public class Exit_app_script : MonoBehaviour
     private const int CONDITION_RANDOM_RATE = 3;
 
     // initial conditions
-    // current_cond is really the current trial number
+    // current_cond is really just another trial counter, but then pulls from the list of conditions
     private int current_cond = 0;
-    private bool practice_mode = true; // true to include the practice trial
+    private bool practice_mode = false; // true to include the practice trial // FIX
 
     // allows for the rate of spawn to be changed in condition checkCondition()
     private List<float> spawn_rate = new List<float>(new float[MAX_PELLETS]);
@@ -38,26 +41,27 @@ public class Exit_app_script : MonoBehaviour
     // counter for number of dropped pellets
     private List<GameObject> dropped_pellets = new List<GameObject>();
 
+    // trial running, spawn rate, and frame variables
     private bool running_ = false;
     private float next_spawn = 0.0f;
     private int current_spawn = 0;
-
     public int current_frame = 0;
 
-    public GameObject foodPrefab, beeFree, beeRestrict;
+    // pellet counter
     public int pellets_dropped = 0;
 
+    // set up for inbetween trials
     public Text ui_text;
     public RawImage ui_image;
 
-    // 
+    // data structures for position and time
     public struct SaveData
     {
-        public List<Vector3> allPositions; //
-        public List<float> allTimeStamps; // 
+        public List<Vector3> allPositions; 
+        public List<float> allTimeStamps; 
     }
 
-    //
+    // data structures for pellet events and frames
     public struct PelletData
     {
         public List<string> pelletEvent;
@@ -65,20 +69,21 @@ public class Exit_app_script : MonoBehaviour
     }
 
     public List<PelletData> pellData = new List<PelletData>();
-    // public List<List<string>> pelletEvent;
-    // public List<SaveData2> saveData2 = new List<SaveData2>();
 
+    // variables for data structures
     public SaveData data_beeFree, data_beeRestrict;
     private bool quitting = false;
 
-    private float timeIni = 0;
+    // old time variable to reset time after each trial
+    //private float timeIni = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        // create list for saving pellet data
+        // create list/variable for saving pellet data
         for (int ii = 0; ii < MAX_PELLETS; ii++)
         {
+            //
             PelletData pD;
             pD.pelletEvent = new List<string>();
             pD.frameNum = new List<int>();
@@ -86,17 +91,11 @@ public class Exit_app_script : MonoBehaviour
             pellData.Add(pD); // 
         }
 
-     //   for (int pelInd = 0; pelInd < 500000; pelInd++)
-     //  {
-     //       SaveData2 sd2 = new SaveData2();
-     //       sd2.pelletEvent = new List<string>(20);
-     //       saveData2.Add(sd2);
-     //   }
-
+        // set up parameters and conditions
         loadParams();
-
         checkCondition();
 
+        // set up practice screen
         ui_image.enabled = true;
         ui_text.text = "Ready to practice?";
 
@@ -126,22 +125,24 @@ public class Exit_app_script : MonoBehaviour
 
     }
 
-    // Update is called once per frame
+    // Update() is called once per frame
     void Update()
     {
-        // ?
-        float timestamp = Time.time - timeIni;
+        //float timestamp = Time.time - timeIni; // brings timestamp back to zero before each trial
+        float timestamp = Time.time; // keeping the time relative to the start of the game, not reseting at each trial
 
-        // ?
+        // if we are currently in a trial
         if (running_)
         {
             current_frame++;
 
+            // allow for escape quitting
             if (Input.GetKey("escape")) Application.Quit();
 
-            if (Time.time > next_spawn && current_spawn<spawn_rate.Count)
+            //
+            if (Time.time > next_spawn && current_spawn < spawn_rate.Count)
             {
-               
+
                 List<int> avail_spawn = new List<int>();
 
                 for (int ii = 0; ii < spawn_cells.transform.childCount; ii++)
@@ -161,7 +162,7 @@ public class Exit_app_script : MonoBehaviour
                     GameObject pel_ = Instantiate(foodPrefab, spawn_cells.transform.GetChild(avail_spawn[rand_ini]).position, Quaternion.identity);
                     pel_.GetComponent<PelletScript>().pelletID = current_spawn - 1;
 
-                    if (!practice_mode){                  
+                    if (!practice_mode) {
                         pel_.GetComponent<PelletScript>().saveToBuffer("SPAWN");
                     }
                 }
@@ -174,108 +175,71 @@ public class Exit_app_script : MonoBehaviour
             // grab position for each player and add to allTimeStamps (x, y coord)
             data_beeFree.allPositions.Add(beeFree.transform.position);
             data_beeRestrict.allPositions.Add(beeRestrict.transform.position);
-           
+
         }
 
+                    // what's this doing?
         else if (!(current_cond == conditions_.Count - 1))
         {
+            // if space bar is pressed between trials
             if (Input.GetKey("space"))
             {
+                // remove the waiting screen and text
                 ui_image.enabled = false;
                 ui_text.text = "";
 
+                // allow for touch behaviors
                 beeFree.GetComponent<TouchScript.Behaviors.Transformer>().enabled = true;
                 beeRestrict.GetComponent<TouchScript.Behaviors.Transformer>().enabled = true;
 
+                // run the game
                 running_ = true;
 
-                timeIni = Time.time;
+                // start of game time
+                //timeIni = Time.time; // time in seconds since game was launched
             }
 
         }
 
         else
         {
+            // allow for quitting with escape key
             if (Input.GetKey("escape")) Application.Quit();
         }
 
     }
 
-    // fixed rate for data saving (50/s)
+    // fixed rate for saving position data (50 samples per second)
     private void FixedUpdate()
     {
-
-        // Alright, so when a round is in session this code should be fine up 
-        // here ** I think. ** What may throw this off is between trials, when 
-        // some of these variables won't be found. We need to run this, see if 
-        // it throws errors, and if not, check that the sampling rate is 
-        // constant (we will need some other timestamp in here to check that)!
-
-        string filetitle = RA_initials + "-" + ParticipantNumber;
-        string path = "Assets/Data/" + filetitle + ".csv";
-
-        StreamWriter writer = new StreamWriter(path, true);
-
-        // if this is the practice round
-        if (current_cond == 0 && practice_mode)
+        // save data only for real trials and only while running
+        if (!(practice_mode) && running_)
         {
-            string header_string = "Date,Participant,RA,Trial,Condition,Timestamp,BeeOldX,BeeOldY,BeeYoungX,BeeYoungY";
-            writer.WriteLine(header_string);
-        }
+            // set up saving location
+            string filetitle = RA_initials + "-" + ParticipantNumber;
+            string path = "Assets/Resources/Data/" + filetitle + "-movement_data.csv";
 
-        // if this is the first real round
-        if (current_cond == 0 && !practice_mode)
-        {
-            string header_string = "Date,Participant,RA,Trial,Condition,Timestamp,BeeOldX,BeeOldY,BeeYoungX,BeeYoungY";
+            // create writer
+            StreamWriter writer = new StreamWriter(path, true);
 
-            // create column for max number of pellets
-            for (int jj = 0; jj < MAX_PELLETS; jj++)
-                header_string = header_string + ",Pellet" + jj + "Event";
+            // create the string
+            string next_line = System.DateTime.Now + "," + ParticipantNumber + ","
+                    + RA_initials + "," + current_trial + ","
+                    + conditions_[current_cond] + "," + Time.time + ","
+                    + beeFree.transform.position.x + ","
+                    + beeFree.transform.position.y + ","
+                    + beeRestrict.transform.position.x + ","
+                    + beeRestrict.transform.position.y;
 
-            writer.WriteLine(header_string);
-        }
-
-        List<List<string>> all_pell_ev = new List<List<string>>();
-
-        for (int ii = 0; ii < MAX_PELLETS; ii++)
-        {
-            List<string> _pell = new List<string>(new string[data_beeFree.allPositions.Count]);
-            all_pell_ev.Add(_pell);
-        }
-
-        for (int pelInd = 0; pelInd < MAX_PELLETS; pelInd++)
-        {
-            for (int frameInd = 0; frameInd < pellData[pelInd].frameNum.Count; frameInd++)
-            {
-                all_pell_ev[pelInd][pellData[pelInd].frameNum[frameInd]] = pellData[pelInd].pelletEvent[frameInd];
-            }
-
-        }
-
-        // new string for pellet data?
-        List<string> pell_ = new List<string>(new string[data_beeFree.allPositions.Count]);
-
-        //
-        for (int ii = 0; ii < pellData[0].frameNum.Count; ii++)
-            pell_[pellData[0].frameNum[ii]] = pellData[0].pelletEvent[ii];
-
-        // saving lines here
-        for (int ii = 0; ii < data_beeFree.allPositions.Count; ii++)
-        {
-            string next_line = System.DateTime.Now + "," + ParticipantNumber + "," + RA_initials + "," + current_trial + "," + conditions_[current_cond]
-                + "," + data_beeFree.allTimeStamps[ii] + "," + data_beeFree.allPositions[ii].x + ","
-                + data_beeFree.allPositions[ii].y + "," + data_beeRestrict.allPositions[ii].x + "," + data_beeRestrict.allPositions[ii].y;
-
-            for (int jj = 0; jj < all_pell_ev.Count; jj++)
-                next_line = next_line + "," + all_pell_ev[jj][ii];
-
+            // write the line
             writer.WriteLine(next_line);
 
+            // close the writer
+            writer.Close();
         }
-        writer.Close();
     }
 
-    // drops a pellet
+    // adding pellet data
     public void addPellet(GameObject pell_)
     {
 
@@ -288,9 +252,9 @@ public class Exit_app_script : MonoBehaviour
         {
             running_ = false;
 
-            // save data if it's not from practice
+            // save pellet data if it's not from practice
             if (!practice_mode){
-                //saveToDisk();// was saveToDisk();
+                saveToDisk();
             }
 
             //RESETS EVERYTHING
@@ -337,6 +301,7 @@ public class Exit_app_script : MonoBehaviour
 
             checkCondition();
 
+            // clear pellet data
             for (int ii = 0; ii < MAX_PELLETS; ii++)
             {
                 pellData[ii].pelletEvent.Clear();
@@ -389,7 +354,7 @@ public class Exit_app_script : MonoBehaviour
                     spawn_rate[ii] = Random.Range(0.5f, 6.0f);
                 break;
 
-            // end
+            // end screen
             case CONDITION_EXPERIMENT_OVER:
 
                 ui_image.enabled = true;
@@ -403,30 +368,26 @@ public class Exit_app_script : MonoBehaviour
 
     }
 
-    // IF FIXEDUPDATE() WORKS, NIX THIS
-    // save data here
+    // save pellet data here at Update() rate, not FixedUpdate()
     void saveToDisk()
     {
 
-        // need something here to make this saving happen at a fixed rate
-        
-
         string filetitle = RA_initials + "-" + ParticipantNumber;
-        string path = "Assets/Resources/Data/"+ filetitle + ".csv";
+        string path = "Assets/Resources/Data/"+ filetitle + "-pellet_data.csv";
         
         StreamWriter writer = new StreamWriter(path, true);
 
         // if this is the practice round
-        if(current_cond ==0 && practice_mode)
-        {
-            string header_string = "Date,Participant,RA,Trial,Condition,Timestamp,BeeOldX,BeeOldY,BeeYoungX,BeeYoungY";
-            writer.WriteLine(header_string);
-        }
+        //if(current_cond ==0 && practice_mode)
+        //{
+        //    string header_string = "Date,Participant,RA,Trial,Condition,Timestamp,BeeOldX,BeeOldY,BeeYoungX,BeeYoungY";
+        //    writer.WriteLine(header_string);
+        //}
 
         // if this is the first real round
         if(current_cond ==0 && !practice_mode)
         {
-            string header_string = "Date,Participant,RA,Trial,Condition,Timestamp,BeeOldX,BeeOldY,BeeYoungX,BeeYoungY";
+            string header_string = "Date,Participant,RA,Trial,Condition,Timestamp";
 
             // create column for max number of pellets
             for (int jj = 0; jj < MAX_PELLETS; jj++)
@@ -458,19 +419,24 @@ public class Exit_app_script : MonoBehaviour
         for(int ii = 0; ii< pellData[0].frameNum.Count; ii++)
             pell_[pellData[0].frameNum[ii]] = pellData[0].pelletEvent[ii];
             
-        // saving lines here
+        // save data
         for (int ii = 0; ii < data_beeFree.allPositions.Count; ii++)
         {
             string next_line = System.DateTime.Now + "," + ParticipantNumber + "," + RA_initials + "," + current_trial + "," + conditions_[current_cond]
-                + "," + data_beeFree.allTimeStamps[ii] + "," + data_beeFree.allPositions[ii].x + ","
-                + data_beeFree.allPositions[ii].y + "," + data_beeRestrict.allPositions[ii].x + "," + data_beeRestrict.allPositions[ii].y;
+                + "," + data_beeFree.allTimeStamps[ii];
 
-            for(int jj =0; jj< all_pell_ev.Count; jj++)
+            /* This loop is the reason for the duplicate time stamps (I think). 
+            * It takes each pellet and creates a line for any pellet event.
+            * If two pellet events happen in the same frame, it will create
+            * two separate lines. We will just need to collapse across duplicate 
+            * timestamps to see all pellet events at the moment. */
+            for (int jj =0; jj< all_pell_ev.Count; jj++)
                 next_line = next_line + "," + all_pell_ev[jj][ii];
 
             writer.WriteLine(next_line);
 
         }
+
         writer.Close();
 
     }
@@ -481,7 +447,6 @@ public class Exit_app_script : MonoBehaviour
         quitting = true;
         string path = "Assets/Resources/Data/RATest.csv";
         StreamWriter writer = new StreamWriter(path, true);
-
 
         for (int ii = 0; ii < data_beeFree.allPositions.Count; ii++)
         {
@@ -503,15 +468,19 @@ public class Exit_app_script : MonoBehaviour
         // what is this doing?
         int partOffSet = 1 + (ParticipantNumber - 1) * 6;
 
+        // read in condition orders
         TextAsset iniFile = Resources.Load<TextAsset>("loadParams/participant_sheet2");
 
+        // create string to split out rows of conditions
         string[] row = iniFile.text.Split(new char[] { '\n' });
 
         for (int ii = 0; ii < 6; ii++)
         {
+            // create string to split columns of conditions
             string[] col = row[partOffSet + ii].Split(new char[] { ',' });
 
             int temptemp;
+            // parse apart conditions from file
             int.TryParse(col[1], out temptemp);
             conditions_.Add(temptemp);
         }
@@ -534,7 +503,5 @@ public class Exit_app_script : MonoBehaviour
          dataSave_.target_x.Add(0);
          dataSave_.target_y.Add(0);*/
     }
-
-
 
 }
