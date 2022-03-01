be8a455c514f39f73dd0f26aff546d24a15fb074
Megan Chiovaro <megan.chiovaro@uconn.edu>
2022-02-28 22:12:36 -0500

    Add in randomized practice round

--- Assets/Resources/loadParams/condition_generation.R
+++ Assets/Resources/loadParams/condition_generation.R
@@ -12,10 +12,10 @@
 set.seed(2022)
 
 # create a new data frame with condition columns
-df <- data.frame(matrix(ncol = 11, nrow = 315))
+df <- data.frame(matrix(ncol = 12, nrow = 315))
 
 # provide column names
-colnames(df) <- c('group_number', 'ie_condition', 
+colnames(df) <- c('group_number', 'ie_condition', 'practice',
                   'td_1', 'td_1', 'td_1', 'td_1', 
                   'td_1', 'td_1', 'com_condition', 
                   'group_size', 'experiment_number')
@@ -28,6 +28,10 @@ df$experiment_number[136:nrow(df)] <- 3
  
 ##### Task demand conditions #####
 
+# for the practice round
+shuffled_practice <- sample(1:3, size = nrow(df), replace = TRUE)
+df$practice <- shuffled_practice
+
 # for all rows
 for (i in 1:nrow(df)){
     
@@ -38,10 +42,10 @@ for (i in 1:nrow(df)){
     sample <- t(as.data.frame(shuffled))
     
     # fill it into a row
-    df[i, c(3:8)] <- sample
+    df[i, c(4:9)] <- sample
     
     # for each column in that row
-    for (j in 3:8){
+    for (j in 4:9){
       
       # if the value is greater than 3
       if(df[i, j] > 3){
@@ -78,22 +82,22 @@ df$ie_condition[31:45] <- 3
 # experiment 1
 df$com_condition[1:45] <- 1
 
-# experiment 2 - ie = 1
+# experiment 2 - ie = ?
 df$com_condition[46:60] <- 1
 df$com_condition[61:75] <- 2
 df$com_condition[76:90] <- 3
 
-# experiment 2 - ie = 2
+# experiment 2 - ie = ?
 df$com_condition[91:105] <- 1
 df$com_condition[106:120] <- 2
 df$com_condition[121:135] <- 3
 
-# experiment 3 - ie = 1
+# experiment 3 - ie = ?
 df$com_condition[136:165] <- 1
 df$com_condition[166:195] <- 2
 df$com_condition[196:225] <- 3
 
-# experiment 3 - ie = 2
+# experiment 3 - ie = ?
 df$com_condition[226:255] <- 1
 df$com_condition[256:285] <- 2
 df$com_condition[286:315] <- 3
@@ -103,22 +107,22 @@ df$com_condition[286:315] <- 3
 # experiments 1 and 2
 df$group_size[1:135] <- 1
 
-# experiment 3 condition 1 - ie 1
+# experiment 3 condition 1 - ie ?
 df$group_size[136:150] <- 1
 df$group_size[166:180] <- 1
 df$group_size[196:210] <- 1
 
-# experiment 3 condition 1 - ie 2
+# experiment 3 condition 1 - ie ?
 df$group_size[226:240] <- 1
 df$group_size[256:270] <- 1
 df$group_size[286:300] <- 1
 
-# experiment 3 condition 2 - ie 1
+# experiment 3 condition 2 - ie ?
 df$group_size[151:165] <- 2
 df$group_size[181:195] <- 2
 df$group_size[211:225] <- 2
 
-# experiment 3 condition 2 - ie 2
+# experiment 3 condition 2 - ie ?
 df$group_size[241:255] <- 2
 df$group_size[271:285] <- 2
 df$group_size[301:315] <- 2
@@ -134,10 +138,13 @@ shuffled_data_2 = df[sample(46:135), ]
 # shuffling experiment 3 rows
 shuffled_data_3 = df[sample(136:315), ]
 
-##### Save data #####
-# shuffled_data_all <- rbind(shuffled_data_1, 
-#                            shuffled_data_2,
-#                            shuffled_data_3)
+##### Bind data #####
+shuffled_data_all <- rbind(shuffled_data_1, 
+                            shuffled_data_2,
+                            shuffled_data_3)
+
+##### Group number #####
+shuffled_data_all$group_number <- seq.int(nrow(shuffled_data_all))
 
 # write shuffled data to file
 write.table(x = shuffled_data_all,
